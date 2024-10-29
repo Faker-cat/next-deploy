7cb529cd7757baf147740ac54be35a6977eb1466
Faker-cat <tatsu.ino1202@gmail.com>
2024-10-29 14:46:35 +0900

    deploy

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,11 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function hello() {
+export default function Hello() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    async function getMessage() {
+      try {
+        const url = "https://fast-api-deploy-inky.vercel.app";
+        const res = await axios.get(url);
+        setMessage(res.data.message);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getMessage();
+  }, []);
+
   return (
     <>
-      <h1>
-        Hello World!!
-      </h1>
-        
+      <h1>{message}</h1>
     </>
   );
 }
+
+
+// export default function hello() {
+//   return (
+//     <>
+//       <h1>
+//         Hello World!!
+//       </h1>
+        
+//     </>
+//   );
+// }
