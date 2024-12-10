a783a581af157e9bc817866c5bae684dd9d11075
Faker-cat <tatsu.ino1202@gmail.com>
2024-12-10 14:27:14 +0900

    login画面6

--- src/SessionProvider/SessionProvider.tsx
+++ src/SessionProvider/SessionProvider.tsx
@@ -1,4 +1,3 @@
-import { Session } from "@supabase/supabase-js";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
